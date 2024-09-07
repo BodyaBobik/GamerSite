519748642e336a3556b6ea0f0e000715ab473e05
BodyaBobik <113145770+BodyaBobik@users.noreply.github.com>
2024-09-07 17:21:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Сайт GameZone завантажено успішно!");
+    // Тут можна додати анімації, форми зворотного зв'язку та інший функціонал
+});
